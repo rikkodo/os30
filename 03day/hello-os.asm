767b6378ba917240a1c8f6f2a0701875625f42d7
rikkodo <ctrlalt.exit@gmail.com>
2018-02-17 21:56:50 +0900

    day3_0a

---
+++ 03day/hello-os.asm
@@ -0,0 +1,76 @@
+; FAT32 FDD
+    ORG 0x7c00
+pbr:
+        JMP     entry
+        NOP
+        DB      "HELLOIPL"
+        DW      512
+        DB      1
+        DW      1
+        DB      2
+        DW      224
+        DW      2880
+        DB      0xf0
+        DW      9
+        DW      18
+        DW      2
+        DD      0
+        DD      2880
+        DB      0,0,0x29
+        DD      0xffffffff
+        DB      "HELLO-OS   "
+        DB      "FAT12   "
+        TIMES   18  DB  0
+
+; MAIN
+; レジスタ初期化処理
+entry:
+        MOV     AX,0
+        MOV     SS,AX
+        MOV     SP,0x7c00
+        MOV     DS,AX
+
+; ディスクの読み込み
+
+        MOV     AX,0x0820   ; OS 読み込み先番地を設定
+        MOV     ES,AX
+        MOV     BX,0        ; アドレスはEX * 16 + BX
+        MOV     CH,0        ; シリンダ  0
+        MOV     DH,0        ; ヘッド    0
+        MOV     CL,2        ; セクタ    2
+
+        MOV     AH,0x02     ; ディスク読み出し
+        MOV     AL,1        ; 1セクタ
+        MOV     DL,0x00     ; Aドライブ
+        INT     0x13        ; ディスクBIOSコール
+        JC      error
+
+; 無限ループ
+fin:
+        HLT
+        JMP     fin
+
+;  error message
+error:
+        MOV     SI,msg
+putloop:
+        MOV     AL,[SI]
+        ADD     SI,1
+        CMP     AL,0
+        JE      fin
+        MOV     AH,0x0e
+        MOV     BX,15
+        INT     0x10
+        JMP     putloop
+
+msg:
+        DB      0x0a, 0x0a
+        DB      "LOAD ERROR"
+        DB      0x0a
+        DB      0
+
+; ブートフラグ設定
+        ;TIMES   0x7dfe-($-$$)  DB  0
+        TIMES   0x1fe-($-$$)  DB  0
+        DB      0x55, 0xaa
+
