f2888efa48b91117f2cb35a1fa865b746048f5f5
rikkodo <ctrlalt.exit@gmail.com>
2018-05-06 20:32:51 +0900

    harib00g video mode

--- 03day/haribote.asm
+++ 03day/haribote.asm
@@ -1,4 +1,8 @@
         ORG     0xc200
+
+        MOV     AL,0x13     ; VGA グラッフィクス 320x200x8bit Color
+        MOV     AH,0x00
+        INT     0x10
 fin:
         HLT
-        jmp     fin
+        JMP     fin
