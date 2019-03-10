d22bcc53114d7fb712d66d11ed98194c43c286bd
rikkodo <ctrlalt.exit@gmail.com>
2019-03-10 17:53:40 +0900

    harib00j hltを呼び出すように設定． // しかし，ほとんど写経になってるな．．．

--- 03day/bootpack.c
+++ 03day/bootpack.c
@@ -1,6 +1,10 @@
+extern void io_hlt(void);
+
+
 void HariMain (void)
 {
     /* hlt */
 fin:
+    io_hlt();  // call nasm func
     goto fin;
 }
---
+++ 03day/nasmfunc.asm
@@ -0,0 +1,10 @@
+; nasmfunc.asm
+
+
+section .text
+    GLOBAL  io_hlt
+
+
+io_hlt:         ; void io_hlt(void);
+    HLT
+    RET
