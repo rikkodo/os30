30224a5c6cc1b40aa575bd3dff23b93351819202
rikkodo <ctrlalt.exit@gmail.com>
2019-03-24 16:43:21 +0900

    harib01b シマシマ模様

--- 04day/bootpack.c
+++ 04day/bootpack.c
@@ -4,9 +4,9 @@ extern void write_mem8(void*, int);
 void HariMain (void)
 {
     int i = 0;
-    for (i = 0xa0000; i< 0xaffff; i++)
+    for (i = 0xa0000; i <= 0xaffff; i++)
     {
-        write_mem8((void*)i, 15);
+        write_mem8((void*)i, i & 0x0f);
     }
     /* hlt */
     for (;;)
