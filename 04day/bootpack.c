0a304de54c5d7d5c905a6d7c74eabe6b069af4bd
rikkodo <ctrlalt.exit@gmail.com>
2019-03-24 17:51:22 +0900

    色番号設定 harib01f

--- 04day/bootpack.c
+++ 04day/bootpack.c
@@ -1,9 +1,22 @@
 extern void io_hlt(void);
-extern void write_mem8(void*, int);
+extern void io_cli(void);
+extern void io_out8(int port, int data);
+extern int  io_load_eflags(void);
+extern void io_store_eflags(int eflags);
+
+#define COL_CNT (16)
+
+static void init_palette(void);
+static void set_palette(int start, int cnt, unsigned char *rgb);
+
 
 void HariMain (void)
 {
     int i = 0;
+    char *p = (char *)0;
+
+    /* パレット初期化 */
+    init_palette();
 
     /* 色ぬり */
     for (i = 0xa0000; i <= 0xaffff; i++)
@@ -11,9 +24,65 @@ void HariMain (void)
         char* p = (char *)i;
         *p = i & 0x0f;
     }
+
     /* hlt */
     for (;;)
     {
         io_hlt();
     }
+}
+
+
+void init_palette(void)
+{
+    /* 色テーブル */
+    static unsigned char table_rgb[COL_CNT * 3] =  /* 3: r, g, b */
+    {
+        0x00, 0x00, 0x00,  /*  0:黒         */
+        0xff, 0x00, 0x00,  /*  1:明るい赤   */
+        0x00, 0xff, 0x00,  /*  2:明るい緑   */
+        0xff, 0xff, 0x00,  /*  3:明るい黄色 */
+        0x00, 0x00, 0xff,  /*  4:明るい青   */
+        0xff, 0x00, 0xff,  /*  5:明るい紫   */
+        0x00, 0xff, 0xff,  /*  6:明るい水色 */
+        0xff, 0xff, 0xff,  /*  7:白         */
+        0xc6, 0xc6, 0xc6,  /*  8:明るい灰色 */
+        0x84, 0x00, 0x00,  /*  9:暗い赤     */
+        0x00, 0x84, 0x00,  /* 10:暗い緑     */
+        0x84, 0x84, 0x00,  /* 11:暗い黄色   */
+        0x00, 0x00, 0x84,  /* 12:暗い青     */
+        0x84, 0x00, 0x84,  /* 13:暗い紫     */
+        0x00, 0x84, 0x84,  /* 14:暗い水色   */
+        0x84, 0x84, 0x84,  /* 15:暗い灰色   */
+    };
+
+    set_palette(0, COL_CNT, table_rgb);
+
+    return;
+}
+
+
+void set_palette(int start, int cnt, unsigned char *rgb)
+{
+    int i = 0;
+    int eflags = 0;
+
+    /* 割り込みフラグ取得 */
+    eflags = io_load_eflags();
+    /* 割り込みの無効化 */
+    io_cli();
+
+    /* 色の格納 */
+    io_out8(0x03c8, start);
+    for (i = start; i < cnt; i++)
+    {
+        io_out8(0x03c9, rgb[i * 3 + 0] / 4);
+        io_out8(0x03c9, rgb[i * 3 + 1] / 4);
+        io_out8(0x03c9, rgb[i * 3 + 2] / 4);
+    }
+
+    /* 割り込みフラグを戻す */
+    io_store_eflags(eflags);
+
+    return;
 }
--- 04day/nasmfunc.asm
+++ 04day/nasmfunc.asm
@@ -2,9 +2,74 @@
 
 
 section .text
-    GLOBAL  io_hlt
+    GLOBAL  io_hlt, io_cli, io_sti, io_stihlt
+    GLOBAL  io_in8,  io_in16,  io_in32
+    GLOBAL  io_out8, io_out16, io_out32
+    GLOBAL  io_load_eflags, io_store_eflags
 
 
-io_hlt:         ; void io_hlt(void);
+io_hlt:                     ; void io_hlt(void);
     HLT
+    RET
+
+io_cli:                     ; void io_cli(void);
+    CLI
+    RET
+
+io_sti:                     ; void io_sti(void);
+    STI
+    RET
+
+io_stihlt:                  ; void io_stihlt(void);
+    STI
+    HLT
+    RET
+
+
+io_in8:                     ; int io_in8(int port);
+    MOV     EDX,[ESP+4]     ; EDX <- port
+    MOV     EAX,0
+    IN      AL,DX           ; AL(= ret(7..0)) <- DX
+    RET
+
+io_in16:                    ; int io_in16(int port);
+    MOV     EDX,[ESP+4]     ; EDX <- port
+    MOV     EAX,0
+    IN      AX,DX           ; AX(= ret(f..0)) <- DX
+    RET
+
+io_in32:                    ; int io_in32(int port);
+    MOV     EDX,[ESP+4]     ; EDX <- port
+    IN      EAX,DX          ; EAX(= ret) <- DX
+    RET
+
+
+io_out8:                    ; int io_out8(int port, int data);
+    MOV     EDX,[ESP+4]     ; EDX <- port
+    MOV     AL,[ESP+8]      ; AL <- data(7..0)
+    OUT     DX,AL
+    RET
+
+io_out16:                   ; int io_out16(int port, int data);
+    MOV     EDX,[ESP+4]     ; EDX <- port
+    MOV     AX,[ESP+8]      ; AX <- data(f..0)
+    OUT     DX,AX
+    RET
+
+io_out32:                   ; int io_out32(int port, int data);
+    MOV     EDX,[ESP+4]     ; EDX <- port
+    MOV     EAX,[ESP+8]     ; EAX <- data
+    OUT     DX,EAX
+    RET
+
+
+io_load_eflags:             ; int io_load_eflags(void);
+    PUSHFD                  ; PUSH EFLAGS
+    POP     EAX             ; EAX <- EFLAGS
+    RET
+
+io_store_eflags:            ; void io_store_eflags(int eflags);
+    MOV     EAX,[ESP+4]     ; EDX <- eflags
+    PUSH    EAX             ; STACKTOP <- EAX
+    POPFD                   ; EFLAGS <- STACKTOP
     RET
