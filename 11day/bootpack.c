42aa597289e39f344951dc3da4c4c7a6a8ec7e75
rikkodo <ctrlalt.exit@gmail.com>
2019-07-31 10:11:56 +0900

    11day

---
+++ 11day/bootpack.c
@@ -0,0 +1,161 @@
+#include "bootpack.h"
+
+
+static void HariMain_in(void);
+
+
+////////////////////////////////////////////////////////////////////////////////
+// main
+void HariMain (void)
+{
+    init_gdtidt();
+    init_pic();
+    io_sti();  /* IDT/PICの初期化が終わったのでCPUの割り込み禁止を解除 */
+
+    HariMain_in();
+    return;
+}
+
+static void HariMain_in (void)
+{
+    /* read boot info */
+    struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+
+    /* key buffer */
+    unsigned char keybuf[KEYBUF_READ_MAX] = {};
+    unsigned char mousebuf[MOUSE_READ_MAX] = {};
+
+    struct MOUSE_DECODE mousedec = {};
+
+    /* mouse cursor */
+    int mx = 0;
+    int my = 0;
+    int i = 0;
+
+    /* memory map */
+    struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+    unsigned int memtotal = 0;
+
+    /* sheet */
+    struct SHEET_CTL *shtctl = 0;
+    struct SHEET *sht_back = 0;
+    struct SHEET *sht_mouse = 0;
+    unsigned char *buf_back = 0;
+    unsigned char buf_mouse[256] = {};
+
+    /* message buffer */
+    char s[128] = {};
+
+    fifo8_init(&keyinfo, KEYBUF_READ_MAX, keybuf);
+    fifo8_init(&mouseinfo, MOUSE_READ_MAX, mousebuf);
+    io_out8(PIC0_IMR, 0xf9); /* PIC1とキーボードを許可(11111001) */
+    io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+
+    init_keyboard();
+    enable_mouse(&mousedec);
+
+    /* メモリ初期化 */
+    memtotal = memtest(0x00400000, 0xbfffffff);
+    memman_init(memman);
+    memman_free(memman, 0x00001000, 0x009e000);  /* 0x00001000 -- 0x0009eff */
+    memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
+    /* パレット初期化 */
+    init_palette();
+
+    /* シート作成 */
+    shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+
+    /* スクリーン初期化 */
+    sht_back = sheet_alock(shtctl);
+    buf_back = (unsigned char*) memman_alloc_4k(memman, binfo->scrny * binfo->scrnx);
+    sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);  // -1 -> 透明色なし
+    init_screen8(buf_back, binfo->scrnx, binfo->scrny);
+    // init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+    sheet_slide(shtctl, sht_back, 0, 0);
+    sheet_updown(shtctl, sht_back, 0);
+
+    /* マウスカーソル初期化 */
+    sht_mouse = sheet_alock(shtctl);
+    sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+    /* マウスカーソルが画面中央になるように座標計算 */
+    mx = (binfo->scrnx - 16) / 2;
+    my = (binfo->scrny - 28 - 16) / 2;
+    init_mouse_cursor8(buf_mouse, 99);
+    sheet_slide(shtctl, sht_mouse, mx, my);
+    sheet_updown(shtctl, sht_mouse, 1);
+
+    mysprintf(s, "memory %dMB    free %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
+    putfonts8_asc(buf_back, binfo->scrnx, 0, 48, COL8_WHITE, s);
+    sheet_refresh(shtctl, sht_back, 0, 48, binfo->scrnx, 64);
+
+    /* hlt */
+    for (;;)
+    {
+        io_cli();
+        if (fifo8_status(&keyinfo) != 0)
+        {
+            i = fifo8_get(&keyinfo);
+            io_sti();
+            mysprintf(s, "key: %02x", i);
+            boxfill8(buf_back, binfo->scrnx, COL8_DARK_CYAN, 0, 16, 32 * 8 - 1, 32 - 1);
+            putfonts8_asc(buf_back, binfo->scrnx, 0, 16, COL8_WHITE, s);
+            sheet_refresh(shtctl, sht_back, 0, 16, 32 * 8, 32);
+        }
+        else if (fifo8_status(&mouseinfo) != 0)
+        {
+            i = fifo8_get(&mouseinfo);
+            io_sti();
+
+            if (mouse_decode(&mousedec, i) == 1)
+            {
+                /* output */
+                mysprintf(s, "[lcr] %3d %3d", mousedec.x, mousedec.y);
+                if ((mousedec.btn & 0x01) != 0)
+                {
+                    s[1] = 'L';
+                }
+                if ((mousedec.btn & 0x02) != 0)
+                {
+                    s[3] = 'R';
+                }
+                if ((mousedec.btn & 0x04) != 0)
+                {
+                    s[2] = 'C';
+                }
+                boxfill8(buf_back, binfo->scrnx, COL8_DARK_CYAN, 0, 32, 32 * 13 - 1, 48 - 1);
+                putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_WHITE, s);
+                sheet_refresh(shtctl, sht_back, 0, 32, 32 * 13, 48);
+
+                mx += mousedec.x;
+                my += mousedec.y;
+                if (mx < 0)
+                {
+                    mx = 0;
+                }
+                if (my < 0)
+                {
+                    my = 0;
+                }
+                if (mx > binfo->scrnx - 16)
+                {
+                    mx = binfo->scrnx - 16;
+                }
+                if (my > binfo->scrny - 16)
+                {
+                    my = binfo->scrny - 16;
+                }
+            }
+
+            mysprintf(s, "(%03d, %03d)", mx, my);
+            boxfill8(buf_back, binfo->scrnx, COL8_DARK_CYAN, 0, 0, 79, 15);
+            putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_WHITE, s);
+            sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
+            sheet_slide(shtctl, sht_mouse, mx, my);  // -> 再描画は関数内部で実施
+        }
+        else
+        {
+            io_stihlt();
+        }
+    }
+}
---
+++ 11day/sheet.c
@@ -0,0 +1,216 @@
+#include "bootpack.h"
+
+/* FLAGS */
+#define SHEET_USE      1 << 0;
+
+static void sheet_refreshsub(const struct SHEET_CTL * ctl, int vx0, int vy0, int vx1, int vy1);
+
+struct SHEET_CTL *shtctl_init(struct MEMMAN * memman, unsigned char *vram, int xsize, int ysize)
+{
+    struct SHEET_CTL *ctl = 0;
+    int i = 0;
+    ctl = (struct SHEET_CTL *)memman_alloc_4k(memman, sizeof(struct SHEET_CTL));
+    if (ctl == 0)
+    {
+        return ctl;
+    }
+
+    ctl->vram = vram;
+    ctl->xsize = xsize;
+    ctl->ysize = ysize;
+    ctl->top = -1;  // no sheet.
+    for (i = 0; i < MAX_SEEHTS; i++)
+    {
+        ctl->sheets0[i].flags = 0;  // unused;
+    }
+
+    return ctl;
+}
+
+struct SHEET *sheet_alock(struct SHEET_CTL * const ctl)
+{
+    struct SHEET *sheet = 0;
+    int i = 0;
+    for (i = 0; i < MAX_SEEHTS; i++)
+    {
+        if (ctl->sheets0[i].flags == 0)
+        {
+            sheet = &(ctl->sheets0[i]);
+            sheet->flags = SHEET_USE;
+            sheet->height = -1;  // not shown;
+            return sheet;
+        }
+    }
+
+    return 0;  // all sheet in use.
+}
+
+void sheet_setbuf(struct SHEET * const sheet, unsigned char *buf, int xsize, int ysize, int col_inv)
+{
+    sheet->buf = buf;
+    sheet->bxsize = xsize;
+    sheet->bysize = ysize;
+    sheet->col_inv = col_inv;
+    return;
+}
+
+void sheet_updown(struct SHEET_CTL * const ctl, struct SHEET * const sheet, int height)
+{
+    int h = 0;
+    int old = sheet->height;
+
+
+    // 範囲外の補正
+    if (height > ctl->top + 1)
+    {
+        height = ctl->top + 1;
+    }
+    if (height < -1)
+    {
+        height = -1;
+    }
+
+    sheet->height = height;
+
+    //  並べ替え
+    if (old > height)
+    {
+        // 以前より奥に行く
+        if (height >= 0)
+        {
+            // 間にあるシートを引き上げる
+            for (h = old; h > height; h--)
+            {
+                ctl->sheets[h] = ctl->sheets[h - 1];
+                ctl->sheets[h]-> height = h;
+            }
+            ctl->sheets[height] = sheet;
+        }
+        else
+        {
+            // 非表示
+            if (ctl->top > old)
+            {
+                for (h = old; h < ctl->top; h++)
+                {
+                    ctl->sheets[h] = ctl->sheets[h + 1];
+                    ctl->sheets[h]->height = h;
+                }
+            }
+            ctl->top--;
+        }
+        sheet_refreshsub(ctl, sheet->vx0, sheet->vy0, sheet->vx0 + sheet->bxsize, sheet->bysize);  // 再描画
+    }
+    else if (old < height)
+    {
+        // 以前より前に行く
+        if (old > 0)
+        {
+            // 間のものを下げる
+            for (h = old; h < height; h++)
+            {
+                ctl->sheets[h] = ctl->sheets[h + 1];
+                ctl->sheets[h]->height = h;
+            }
+            ctl->sheets[height] = sheet;
+        }
+        else
+        {
+            // 非表示から表示
+            for (h = ctl->top; h >= height; h--)
+            {
+                ctl->sheets[h + 1] = ctl->sheets[h];
+                ctl->sheets[h + 1]->height = h + 1;
+            }
+            ctl->sheets[height] = sheet;
+            ctl->top++;
+        }
+        sheet_refreshsub(ctl, sheet->vx0, sheet->vy0, sheet->vx0 + sheet->bxsize, sheet->bysize);  // 再描画
+    }
+    return;
+}
+
+void sheet_refresh(const struct SHEET_CTL *ctl, struct SHEET * const sht, int bx0, int by0, int bx1, int by1)
+{
+    if (sht->height >= 0)
+    {
+        sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
+    }
+    return;
+}
+
+static void sheet_refreshsub(const struct SHEET_CTL * ctl, int vx0, int vy0, int vx1, int vy1)
+{
+    int h = 0;
+    int bx = 0;
+    int by = 0;
+    int bx0 = 0;
+    int by0 = 0;
+    int bx1 = 0;
+    int by1 = 0;
+    int vx = 0;
+    int vy = 0;
+
+    unsigned char *buf = 0;
+    unsigned char col = 0;
+    unsigned char *vram = ctl->vram;
+
+    struct SHEET * sht = 0;
+
+    for (h = 0; h <= ctl->top; h++)
+    {
+        sht = ctl->sheets[h];
+        buf = sht->buf;
+        bx0 = vx0 - sht->vx0;
+        by0 = vy0 - sht->vy0;
+        bx1 = vx1 - sht->vx0;
+        by1 = vy1 - sht->vy0;
+        if (bx0 < 0) {bx0 = 0;}
+        if (by0 < 0) {by0 = 0;}
+        if (bx1 > sht->bxsize) {bx1 = sht->bxsize;}
+        if (by1 > sht->bysize) {by1 = sht->bysize;}
+        for (by = by0; by < by1; by++)
+        {
+            vy = sht->vy0 + by;
+            for (bx = bx0; bx < bx1; bx++)
+            {
+                vx = sht->vx0 + bx;
+                col = buf[by * sht->bxsize + bx];
+                if (col != sht->col_inv)
+                {
+                    vram[vy * ctl->xsize + vx] = col;
+                }
+            }
+        }
+    }
+
+    return;
+}
+
+
+void sheet_slide(struct SHEET_CTL * const ctl, struct SHEET * const sht, int vx0, int vy0)
+{
+    int pre_vx0 = sht->vx0;
+    int pre_vy0 = sht->vy0;
+    sht->vx0 = vx0;
+    sht->vy0 = vy0;
+    if (sht->height >= 0)
+    {
+        // 表示中ならば再描画
+        sheet_refreshsub(ctl, pre_vx0, pre_vy0, pre_vx0 + sht->bxsize, pre_vy0 + sht->bysize);
+        sheet_refreshsub(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
+    }
+    return;
+}
+
+void sheet_free(struct SHEET_CTL * const ctl, struct SHEET * const sht)
+{
+    if (sht->height >= 0)
+    {
+        // 表示中の場合消す。
+        sheet_updown(ctl, sht, -1);
+    }
+
+    sht->flags = 0;
+    return;
+}
